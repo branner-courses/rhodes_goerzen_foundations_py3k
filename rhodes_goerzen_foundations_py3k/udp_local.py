d89cf78e94ca5e2d5b148bea2cb11b79b82697bd
David Branner <brannerchinese@gmail.com>
2014-07-08 22:46:34 -0400

    update udp_local.py

--- rhodes_goerzen_foundations_py3k/udp_local.py
+++ rhodes_goerzen_foundations_py3k/udp_local.py
@@ -16,15 +16,18 @@ if sys.argv[1:] == ['server']:
     print('Listening at {}'.format(s.getsockname()))
     while True:
         data, address = s.recvfrom(MAX)
-        print('The client at {} says {}'.format(address, data.decode('utf-8')))
-        s.sendto(b'Your data was {} bytes'.format(len(data)), address)
+        print('The client at {} says {}'.
+                format(address, str(data, 'utf-8')))
+        s.sendto(bytes('Your data was {} bytes'.format(len(data)), 'utf-8'),
+                address)
 
 elif sys.argv[1:] == ['client']:
     print('Address before sending: {}'.format(s.getsockname()))
     s.sendto(b'This is my message', ('127.0.0.1', PORT))
     print('Address after sending: {}'.format(s.getsockname()))
     data, address = s.recvfrom(MAX) # overly promiscuous - see text!
-    print('The server {} says {}'.format(address, data.decode('utf-8')))
+    print('The server {} says {}'.
+            format(address, str(data, 'utf-8')))
 
 else:
     sys.stderr.write('usage: udp_local.py server|client')
